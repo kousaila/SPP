933df7c12139a57e1a516a610ecb75d817def833
Tidjet Kousaila <kousaila.tidjet@etudiant.univ-rennes1.fr>
2020-03-04 15:14:42 +0100

    ajout d'image

---
+++ SPP/src/tp56/SimpleImageProcessingExample.java
@@ -0,0 +1,49 @@
+package tp56;
+
+/**
+ * SimpleImageProcessingExample.java
+ * 
+ * Time-stamp: <2017-03-09 22:26:51 ftaiani>
+ * @author Francois Taiani   <francois.taiani@irisa.fr>
+ * $Id$
+ * 
+ */
+
+import javax.imageio.ImageIO;
+import java.awt.image.*;
+import java.awt.Color ;
+import java.io.File;
+
+/**
+ * @author Francois Taiani   <francois.taiani@irisa.fr>
+ */
+public class SimpleImageProcessingExample {
+
+  static public void main(String[] args) throws Exception {
+    // reading image in
+    BufferedImage inImg  = ImageIO.read(new File("TEST_IMAGES/15226222451_5fd668d81a_c.jpg"));
+    // creating new image
+    BufferedImage outImg = new BufferedImage(inImg.getWidth(),
+                                             inImg.getHeight(),
+                                             BufferedImage.TYPE_INT_RGB);
+    // generating new image from original
+    for (int x = 0; x < inImg.getWidth(); x++) {
+      for (int y = 0; y < inImg.getHeight(); y++) {
+          int rgb    = inImg.getRGB(x,y);
+          // extracting red, green and blue components from rgb integer
+          int red    = (rgb >> 16) & 0x000000FF;
+          int green  = (rgb >>  8) & 0x000000FF;
+          int blue   = (rgb      ) & 0x000000FF;
+          // computing new color from extracted components
+          int newRgb = ( ( (green << 8) | blue ) << 8 ) | red ; // rotating RGB values
+          outImg.setRGB(x,y,newRgb);
+      } // EndFor y
+    } // EndFor x
+
+    // writing out new image
+    File f = new File("TEST_IMAGES/tmp.png");
+    ImageIO.write(outImg, "png", f);
+    
+  } // EndMain
+
+} // EndClass SimpleImageProcessingExample
--- SPP/src/tp56/singlethread.java
+++ SPP/src/tp56/singlethread.java
@@ -1,5 +1,49 @@
 package tp56;
 
+import java.awt.image.BufferedImage;
+
 public class singlethread implements IFilter,IImageFilteringEngine{
+
+	@Override
+	public void loadImage(String inputImage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void writeOutPngImage(String outFile) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setImg(BufferedImage newImg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public BufferedImage getImg() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void applyFilter(IFilter someFilter) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getMargin() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void applyFilterAtPoint(int x, int y, BufferedImage imgIn, BufferedImage imgOut) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
